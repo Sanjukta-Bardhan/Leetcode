c7b5b3868e4f26f8df0b33fbf3156165e448cab2
SANJUKTA <sanjuktabardhan5102@gmail.com>
2025-09-14 19:31:05 +0530

    Time: 87 ms (17.09%) | Memory: 66.7 MB (63.25%) - LeetSync

---
+++ 2804-compact-object/compact-object.js
@@ -0,0 +1,11 @@
+var compactObject = function(obj) {
+    const compactObj = Array.isArray(obj) ? [] : {};
+    for(const key in obj) {
+        let elem = obj[key];
+        if(elem) {
+            if(typeof elem === "object") elem = compactObject(elem);
+            Array.isArray(obj) ? compactObj.push(elem) : compactObj[key] = elem;
+        }
+    }
+    return compactObj;
+};
