dcf515f10a521d89bca9a38337ae0a1c04284728
SANJUKTA <sanjuktabardhan5102@gmail.com>
2025-09-17 11:30:37 +0530

    Time: 51 ms (28.21%) | Memory: 56.1 MB (55.16%) - LeetSync

---
+++ 2798-chunk-array/chunk-array.js
@@ -0,0 +1,11 @@
+let chunk= (arr, size) =>{
+  const chunkedArray = [];
+  let index = 0;
+
+  while (index < arr.length) {
+    chunkedArray.push(arr.slice(index, index + size));
+    index += size;
+  }
+
+  return chunkedArray;
+}
